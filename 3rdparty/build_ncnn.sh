5dcc467674da869655475a0138af2814ae71ad8b
Cyrus Behroozi <cyrus.behr@gmail.com>
2020-09-18 16:37:26 -0700

    Added build script for building opencv

--- 3rdparty/build_ncnn.sh
+++ 3rdparty/build_ncnn.sh
@@ -20,14 +20,16 @@ cd ..
 
 # Build for arm32
 cd build_arm32   
-cmake -D NCNN_BUILD_TOOLS=OFF -D NCNN_VULKAN=OFF -D CMAKE_BUILD_TYPE=Release -D NCNN_DISABLE_RTTI=OFF -D CMAKE_TOOLCHAIN_FILE=../toolchains/arm-linux-gnueabihf.toolchain.cmake -D NCNN_BUILD_EXAMPLES=OFF ..
+cmake -D NCNN_BUILD_TOOLS=OFF -D NCNN_VULKAN=OFF -D CMAKE_BUILD_TYPE=Release -D NCNN_DISABLE_RTTI=OFF \
+-D CMAKE_TOOLCHAIN_FILE=../toolchains/arm-linux-gnueabihf.toolchain.cmake -D NCNN_BUILD_EXAMPLES=OFF ..
 make -j$(nproc)
 make DESTDIR=packaged install
 cd ..
 
 # Build for arm64
 cd build_arm64
-cmake -D NCNN_BUILD_TOOLS=OFF -D NCNN_VULKAN=OFF -D CMAKE_BUILD_TYPE=Release -D NCNN_DISABLE_RTTI=OFF -D CMAKE_TOOLCHAIN_FILE=../toolchains/aarch64-linux-gnu.toolchain.cmake -D NCNN_BUILD_EXAMPLES=OFF ..
+cmake -D NCNN_BUILD_TOOLS=OFF -D NCNN_VULKAN=OFF -D CMAKE_BUILD_TYPE=Release -D NCNN_DISABLE_RTTI=OFF \
+-D CMAKE_TOOLCHAIN_FILE=../toolchains/aarch64-linux-gnu.toolchain.cmake -D NCNN_BUILD_EXAMPLES=OFF ..
 make -j$(nproc)
 make DESTDIR=packaged install
 cd ..
