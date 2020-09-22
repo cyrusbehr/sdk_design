6c7ae5ced83b4a0ecc16c6d9e584a6e7061e2550
Cyrus Behroozi <cyrus.behr@gmail.com>
2020-09-22 11:26:35 -0700

    Added documentation

--- include/my_sdk.h
+++ include/my_sdk.h
@@ -6,26 +6,42 @@
 
 namespace sdk {
 
+    /** Error codes returned by methods */
     enum class ErrorCode {
         NO_ERROR,
         FAILED
     };
 
     struct Point {
+        /** Coordinate along the horizontal axis, or pixel column */
         int x;
+        /** Coordinate along the vertical axis, or pixel row */
         int y;
     };
 
     struct FaceBoxAndLandmarks {
+        /**  The top left corner of the bounding box */
         Point topLeft;
+        /**  The bottom right corner of the bounding box */
         Point bottomRight;
+        /**  The facial landmark points: left eye, right eye, nose, left mouth corner, right mouth corner */
         std::array<Point, 5> landmarks;
     };
 
     class MySDK {
     public:
         MySDK();
+
         ~MySDK();
+        /**
+         *
+         * Detect all the faces in an image and return the corresponding face bounding boxes and landmarks
+         *
+         * @param imgPath path to the image which will be used for face detection.
+         * @param faceDetected indicates if one or more faces were detected in the image.
+         * @param fbAndLandmarksVec the resulting bounding boxes and landmarks for the detected faces.
+         * @return error code, see ErrorCode
+         */
         ErrorCode getFaceBoxAndLandmarks(const std::string& imgPath, bool& faceDetected, std::vector<FaceBoxAndLandmarks>& fbAndLandmarksVec);
 
     private:
